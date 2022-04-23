85d304aa41d1ef597de9c56a59a5c95d8e1b3f0a
Sumedh Pendurkar <sumedh.pendurkar@gmail.com>
2022-04-23 15:22:18 -0500

    Update w3_code.py

--- src/w3_code.py
+++ src/w3_code.py
@@ -5,5 +5,5 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 ## dimensions [size][x/y][784][1]
 #n = ### YOUR CODE HERE ###
-net = net.SGD([784, n, 10])
+net = net.Network([784, n, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
